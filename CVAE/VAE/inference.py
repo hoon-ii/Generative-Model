92aa092827485b24d36bcdba1ccb41394ce89663
hwawon <hwawon03@uos.ac.kr>
2024-11-19 20:02:45 +0000

    VAE

---
+++ CVAE/VAE/inference.py
@@ -0,0 +1,146 @@
+#%%
+import os
+import torch
+import argparse
+import importlib
+from torchvision.utils import save_image
+import torch
+import matplotlib.pyplot as plt
+from modules.utils import set_random_seed
+from evaluate.evaluate import convert2png, evalutae
+
+#%%
+import sys
+import subprocess
+try:
+    import wandb
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
+    with open("../wandb_api.txt", "r") as f:
+        key = f.readlines()
+    subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
+    import wandb
+
+project = "VAE"
+# entity = "hwawon" # put your WANDB username
+
+run = wandb.init(
+    project=project, 
+    # entity=entity, 
+    tags=["inference"], # put tags of this python project
+)
+# %%
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+    
+#%%
+def get_args(debug=False):
+    parser = argparse.ArgumentParser('parameters')
+
+    parser.add_argument("--num_samples", type=int, default=1000)
+
+    parser.add_argument('--ver', type=int, default=0, 
+                        help='model version number')
+    
+    parser.add_argument('--dataset', type=str, default='MNIST', 
+                        help="""
+                        Dataset options: MNIST, CIFAR10
+                        """)
+    parser.add_argument('--latent_dim', type=int, default=64, 
+                        help='Dimension of latent space.')
+
+    parser.add_argument("--FID_size",type=int, default=1024)
+    parser.add_argument("--dims", type=int, default=2048)
+
+
+    if debug:
+        return parser.parse_args(args=[])
+    else:    
+        return parser.parse_args()
+    
+
+#%%
+def main():
+    #%%
+    config = vars(get_args(debug=True))
+    #%%
+    """model load"""
+    base_name = f"{config['dataset']}_{config['latent_dim']}" 
+    model_name = f"VAE_{base_name}"
+    artifact = wandb.use_artifact(
+        f"{project}/{model_name}:v{config['ver']}",
+        type='model')
+    for key, item in artifact.metadata.items():
+        config[key] = item
+    model_dir = artifact.download()
+    model_name = [x for x in os.listdir(model_dir) if x.endswith(f"{config['seed']}.pth")][0]
+    #%%
+    config["device"] = torch.cuda.is_available()
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    config['device'] = device
+    set_random_seed(config["seed"])
+    wandb.config.update(config)
+    #%%
+    """dataset"""
+    dataset_module = importlib.import_module('datasets.preprocess')
+    importlib.reload(dataset_module)
+    train_dataset = dataset_module.CustomMNIST(train=True)
+    test_dataset = dataset_module.CustomMNIST(train=False)    
+    #%%
+    """model"""
+    model_module = importlib.import_module('modules.model')
+    importlib.reload(model_module)
+    model = model_module.VAE(config, train_dataset.EncodedInfo).to(device)
+    
+    if config["device"]:
+        model.load_state_dict(
+            torch.load(
+                model_dir + "/" + model_name
+            )
+        )
+    else:
+        model.load_state_dict(
+            torch.load(
+                model_dir + "/" + model_name,
+                map_location=torch.device("cpu"),
+            )
+        )
+    model.eval()
+    #%%
+    """number of model parameters"""
+    count_parameters = lambda model: sum(p.numel() for p in model.parameters() if p.requires_grad)
+    model_params = count_parameters(model)
+    print(f"Number of Parameters: {model_params/1000000:.1f}M")
+    wandb.log({"Number of Parameters": model_params /1000000})
+    #%%
+    """generation"""
+    # ax = model.generate(test_dataset)
+    #%%
+    """ Image Generation """
+    #%%
+    num_samples = config['num_samples']
+    generated_images = model.generate(train_dataset, num_samples, device)
+
+    output_dir = f"./generated_samples/{model_name}"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    for idx, img in enumerate(generated_images):
+        file_name = f"{config['dataset']}_{idx+1:04d}.png"
+        save_image(img, os.path.join(output_dir, file_name), normalize=True)
+    wandb.log({"generated_images dir": output_dir})
+
+    origin_png_dir = f"./{config['dataset']}_png_dir"
+    convert2png(train_dataset, origin_png_dir)
+    fid_score = evalutae(config, origin_png_dir, output_dir)
+    print("fid_score >>> ",fid_score)
+    wandb.log({"FID": fid_score})
+#%% 
+if __name__ == "__main__":
+    main()  
---
+++ CVAE/VAE/modules/model.py
@@ -0,0 +1,112 @@
+#%%
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import math
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+#%% VAE(fc lyaer로 구성)
+class VAE(nn.Module):
+    def __init__(self, config, EncodedInfo):
+        super(VAE, self).__init__()
+        self.config = config
+        self.channels = EncodedInfo.channels
+        self.height = EncodedInfo.height
+        self.width = EncodedInfo.width
+        self.input_dim = self.channels*self.height*self.width
+
+        # Encoder
+        en = []
+        in_dim = self.input_dim
+
+        for h in config["hidden_dims"]:
+            en.append(nn.Linear(in_dim, h))
+            en.append(nn.ReLU())
+            in_dim = h
+        en.append(nn.Linear(h, config["latent_dim"] * 2)) # mu, logvar 공간을 마련하기 위해 *2 해줌
+
+        # 신경망을 self.encoder 속성으로 정의
+        self.encoder = nn.Sequential(*en)
+        
+        # Decoder
+        de = []
+        in_dim = config["latent_dim"]
+        for h in reversed(config["hidden_dims"]):
+            de.append(nn.Linear(in_dim, h))
+            de.append(nn.ReLU())
+            in_dim = h
+        de.append(nn.Linear(h, self.input_dim))
+        de.append(nn.Sigmoid())
+
+        # 신경망을 self.decoder 속성으로 정의
+        self.decoder = nn.Sequential(*de)
+
+    def encode(self, x):
+        encoded = x.view(-1, self.input_dim) 
+        mu_logvar = self.encoder(encoded)
+        return mu_logvar
+        
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+    
+    def decode(self, z):
+        decoded = self.decoder(z)
+        decoded = decoded.view(-1, self.channels, self.height, self.width)
+        return decoded
+    
+    def forward(self, x):
+        mu_logvar = self.encode(x)
+        mu, logvar = mu_logvar.chunk(2, dim = 1)
+        z = self.reparameterize(mu, logvar)
+        recon = self.decode(z)
+        return recon, mu, logvar
+    
+    # def generate(self, test_dataset, device):
+    #     test_dataloader = DataLoader(
+    #         test_dataset, 
+    #         batch_size=64,
+    #     )
+    #     batch, label = next(iter(test_dataloader))
+        
+    #     with torch.no_grad():
+    #         batch, label = batch.to(device), label.to(device)
+    #         recon, mu, logvar = self(batch)
+       
+    #     grid = gridspec.GridSpec(3, 3)
+    #     plt.figure(figsize = (10, 10))
+    #     plt.subplots_adjust(wspace = 0.5, hspace = 0.5)
+    #     for i in range(9):    
+    #         ax = plt.subplot(grid[i])
+    #         plt.imshow(recon[i].reshape(self.height, self.width).cpu().detach().numpy(), cmap='gray')
+    #         ax.get_xaxis().set_visible(False)
+    #         ax.get_yaxis().set_visible(False)
+    #         ax.set_title('label : {}'.format(label[i]))
+
+    #     return ax
+
+    # def generate(self, test_dataset, num_samples, device):
+    #     test_dataloader = DataLoader(
+    #         test_dataset, 
+    #         batch_size=num_samples,
+    #     )
+    #     batch, _ = next(iter(test_dataloader))
+        
+    #     with torch.no_grad():
+    #         batch = batch.to(device)
+    #         generated_images, _, _ = self(batch)
+    #     return generated_images
+    
+
+    def generate(self, train_dataset, num_samples, device):
+        train_dataloader = DataLoader(
+            train_dataset, 
+            batch_size=num_samples,
+        )
+        batch, _ = next(iter(train_dataloader))
+        
+        with torch.no_grad():
+            batch = batch.to(device)
+            generated_images, _, _ = self(batch)
+        return generated_images
